b2b911e48746f2911319b1c58645ed39a72accfe
Arpit Garg <gargsahabji1@gmail.com>
2022-05-20 17:39:00 +0530

    jwt implementation added in swagger

--- src/main/java/com/garg/blog/config/SwaggerConfig.java
+++ src/main/java/com/garg/blog/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.garg.blog.config;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,23 +11,43 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig {
 
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+	private List<SecurityContext> securityContexts() {
+		return Arrays.asList(SecurityContext.builder().securityReferences(securityReferences()).build());
+	}
+
+	private List<SecurityReference> securityReferences() {
+		AuthorizationScope scopes = new AuthorizationScope("global", "accessEverything");
+		return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[] { scopes }));
+	}
+
+	private ApiKey apiKeys() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+
 	@Bean
 	public Docket getDocket() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo()).select()
-				.apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo()).securityContexts(securityContexts())
+				.securitySchemes(Arrays.asList(apiKeys())).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
 	}
 
 	private ApiInfo getApiInfo() {
 		return new ApiInfo("Blogging API", "This project is developed by Arpit Garg", "1.0", "Terms of Service",
-				new Contact("Arpit Garg", "https://github.com/Garg-Sahab-Ji/Garg-Sahab-Ji", "gargsahabji1@gmail.com"), "License of API", "API License URL",
-				Collections.emptyList());
+				new Contact("Arpit Garg", "https://github.com/Garg-Sahab-Ji/Garg-Sahab-Ji", "gargsahabji1@gmail.com"),
+				"License of API", "API License URL", Collections.emptyList());
 	}
 }
